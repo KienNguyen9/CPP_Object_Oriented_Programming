937e47447f05a08474dd7deec92461dd8bee3b9f
KienNguyen9 <kierannguyen98@gmail.com>
2023-11-07 12:43:38 +0700

    Edit name of Units

---
+++ L02_Class_Object/Unit_10_Destructor_Ham_Huy/README.md
@@ -0,0 +1,8 @@
+# Destructor - Hàm hủy
+Dùng để hủy dữ liệu, thu hồi vùng nhớ
+trong chương trình lớn cần hủy chủ động 
+
+Các object được cấp phát động, có thể hủy chủ động bằng cú pháp:
+```C++
+delete name_Object; // Thu hồi vùng nhớ cấp phát cho name_Object
+```
---
+++ L02_Class_Object/Unit_10_Destructor_Ham_Huy/main.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file main.cpp
+ * @author Kien Nguyen (you@domain.com)
+ * @brief Hàm hủy dùng để làm gì, các dùng hàm hủy
+ * @version 0.1
+ * @date 2023-11-06
+ * 
+ * @copyright Copyright (c) 2023
+ */
+#include <iostream>
+#include <string>
+#include <vector> // thư viện kiểu dư liệu vector
+//Kiểu dữ liệu Bodysize
+struct BodySize 
+{   
+    //Các kiểu nguyên thủy (primitive)
+    int chieuCao;
+    float weight;
+    //Các kiểu dư liệu nâng cao
+    int BodySize3v[3];  // kiểu mảng - array 
+    std::vector<int> body3Size; // kiểu vector - một vector các số nguyên, số lượng phần tử linh động
+};
+/**
+* Lớp woman - VD dụ destructor như sau
+* Tạo ra class woman 
+**/
+
+class woman 
+{
+    public: 
+    // 2 thuộc tính là tên và tuổi 
+    std::string fullname;
+    int age;
+    // Tên hàm trùng với tên class
+    woman()
+    {
+        fullname = "Unknow";
+        age = 0;
+    }
+    //Tên hàm trung với tên class, có thêm đối số
+    woman(std::string fn, int a)
+    {
+        std::cout<<"Create a new woman\n";
+        fullname = fn;
+        age = a;
+    }
+    // Hàm hủy
+    ~woman() // Thêm "~" trước tên hàm, trùng với tên class để tạo hàm hủy
+    {
+        
+    }
+    void makeUp(){}
+    void introduce(){}
+    private:
+};
+
+
+int main()
+{
+    
+    woman *jenny = new woman("Jenny Chu",24); 
+    delete jenny;
+}
