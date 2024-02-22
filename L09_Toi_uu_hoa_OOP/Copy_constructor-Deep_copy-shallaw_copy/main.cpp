a6618be9fbba68ac479f891138a4686ef4fe28ae
KienNguyen9 <kierannguyen98@gmail.com>
2024-02-22 21:33:25 +0700

    Upload lý thuyết tối ưu hóa trong OOP

---
+++ L09_Toi_uu_hoa_OOP/Copy_constructor-Deep_copy-shallaw_copy/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Kien Nguyen
+ * @brief Các cách tối ưu hóa OOP
+ * 1. Copy constructor 
+ *      Copy từ object này sang object khác
+ *      Khi nào cần dùng copy constructor 
+ *
+ * @version 0.1
+ * @date 2024-02-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
